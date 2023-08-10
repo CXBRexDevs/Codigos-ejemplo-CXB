59ead5da4b3b63a2d498f96f39f4d8ede94e3d54
CXBRexDevs <135280799+CXBRexDevs@users.noreply.github.com>
2023-08-10 16:11:02 -0500

    Update main.c

--- BusCAN/firmware/src/main.c
+++ BusCAN/firmware/src/main.c
@@ -10,7 +10,7 @@
 #include <stdarg.h>
 #include "definitions.h"                // SYS function prototypes
 
-/* Application's state machine enum */
+/* Enumeración de los estados */
 typedef enum
 {
     APP_STATE_CAN_RECEIVE,
@@ -21,15 +21,15 @@ typedef enum
     APP_STATE_CAN_XFER_ERROR
 } APP_STATES;
 
-/* set format attribute for the vararg function */
+/* Establecer el formato del atributo vararg */
 void PrintFormattedData (const char * format, ...) __attribute__ ((format (printf, 1, 2)));
 
-/* Variable to save application state */
+/* Variable para guardar el estado de la aplicación */
 static APP_STATES state = APP_STATE_CAN_USER_INPUT;
-/* Variable to save Tx/Rx transfer status and context */
+/* Variable para guardar el estado de transmisión Tx/Rx y su contexto */
 static uint32_t status = 0;
 static uint32_t xferContext = 0;
-/* Variable to save Tx/Rx message */
+/* Variable para guardar los mensajes Tx/Rx */
 static uint32_t messageID = 0;
 static uint8_t message[8];
 static uint8_t messageLength = 0;
@@ -40,33 +40,12 @@ static uint16_t timestamp = 0;
 static CAN_MSG_RX_ATTRIBUTE msgAttr = CAN_MSG_RX_DATA_FRAME;
 uint32_t recivedData;
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Local functions
-// *****************************************************************************
-// *****************************************************************************
 
-/* This function will be called by CAN PLIB when transfer is completed */
-// *****************************************************************************
-/* void APP_CAN_Callback(uintptr_t context)
-
-  Summary:
-    Function called by CAN PLIB upon transfer completion
-
-  Description:
-    This function will be called by CAN PLIB when transfer is completed.
-    In this function, current state of the application is obtained by context
-    parameter. Based on current state of the application and CAN error
-    state, next state of the application is decided.
-
-  Remarks:
-    None.
-*/
 void APP_CAN_Callback(uintptr_t context)
 {
     xferContext = context;
 
-    /* Check CAN Status */
+    /* Revice el estado del CAN */
     status = CAN1_ErrorGet();
 
     if ((status & (CAN_ERROR_TX_RX_WARNING_STATE | CAN_ERROR_RX_WARNING_STATE |
@@ -94,8 +73,8 @@ void APP_CAN_Callback(uintptr_t context)
 void print_menu(void)
 {
     printf(" ------------------------------ \r\n");   
-    printf(" Press '1' to Transmit message \r\n");
-    printf(" Press '2' to Receive message \r\n");
+    printf(" Presione '1' para transmitir \r\n");
+    printf(" Presione '2' para recibir \r\n");
 }
 
 void PrintFormattedData (const char * format, ...)
@@ -126,18 +105,18 @@ int main ( void )
     
     print_menu();
     
-    /* Prepare the message to send*/
+    /* Prepara el mensaje a enviar*/
     messageID = 0x09;
     messageLength = 8;
     SPI1_Read(&recivedData,32);
-    message[0]=1;
-    message[1]=2;
-    message[2]=3;
-    message[3]=4;
-    message[4]=5;
-    message[5]=6;
-    message[6]=7;
-    message[7]=8;
+    message[0]=0x01;
+    message[1]=0x02;
+    message[2]=0x03;
+    message[3]=0x0A;
+    message[4]=0x0B;
+    message[5]=0x0C;
+    message[6]=0x0D;
+    message[7]=0x0E;
     
     while ( true )
     {
@@ -149,27 +128,27 @@ int main ( void )
             switch (user_input)
             {
                 case '1': 
-                    printf(" Transmitting Message: ");                    
+                    printf(" Transmitiendo: ");                    
                     CAN1_CallbackRegister( APP_CAN_Callback, (uintptr_t)APP_STATE_CAN_TRANSMIT, 0 );
                     state = APP_STATE_CAN_IDLE;
                     if (CAN1_MessageTransmit(messageID, messageLength, message, 0, CAN_MSG_TX_DATA_FRAME) == false)
                     {
-                        printf("CAN1_MessageTransmit request has failed\r\n");
+                        printf("CAN1_MessageTransmit petición fallida\r\n");
                     }             
                     break;
                 case '2':
-                    printf(" Waiting for message: \r\n");
+                    printf(" Esperando mensaje: \r\n");
                     CAN1_CallbackRegister( APP_CAN_Callback, (uintptr_t)APP_STATE_CAN_RECEIVE, 1 );
                     state = APP_STATE_CAN_IDLE;
                     memset(rx_message, 0x00, sizeof(rx_message));
                     /* Receive New Message */
                     if (CAN1_MessageReceive(&rx_messageID, &rx_messageLength, rx_message, &timestamp, 1, &msgAttr) == false)  
                     {
-                        printf("CAN1_MessageReceive request has failed\r\n");
+                        printf("CAN1_MessageReceive petición fallida\r\n");
                     }
                     break;
                 default:
-                    printf(" Invalid Input \r\n");
+                    printf(" Comando invalido \r\n");
                     break;
             }
         }
@@ -187,10 +166,10 @@ int main ( void )
                 if ((APP_STATES)xferContext == APP_STATE_CAN_RECEIVE)
                 {
                     /* Print message to Console */
-                    printf(" New Message Received    \r\n");
+                    printf(" Mensaje Receivido    \r\n");
                     uint8_t length = rx_messageLength;
-                    PrintFormattedData(" Message - Timestamp : 0x%x ID : 0x%x Length : 0x%x ", (unsigned int)timestamp, (unsigned int) rx_messageID,(unsigned int) rx_messageLength);
-                    printf("Message : ");
+                    PrintFormattedData(" Mensaje - Timestamp : 0x%x ID : 0x%x Long : 0x%x ", (unsigned int)timestamp, (unsigned int) rx_messageID,(unsigned int) rx_messageLength);
+                    printf("Mensaje : ");
                     while(length)
                     {
                         PrintFormattedData("0x%x ", rx_message[rx_messageLength - length--]);
@@ -199,7 +178,7 @@ int main ( void )
                 } 
                 else if ((APP_STATES)xferContext == APP_STATE_CAN_TRANSMIT)
                 {
-                    printf("Success \r\n");
+                    printf("Exito \r\n");
                 }
                 LED_Toggle();
                 print_menu();
@@ -210,11 +189,11 @@ int main ( void )
             {
                 if ((APP_STATES)xferContext == APP_STATE_CAN_RECEIVE)
                 {
-                    printf("Error in received message");
+                    printf("Error en el mensaje recivido");
                 }
                 else
                 {
-                    printf("Failed \r\n");
+                    printf("Fallo \r\n");
                 }
                 print_menu();
                 state = APP_STATE_CAN_USER_INPUT;
